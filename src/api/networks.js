bbe6d1577be5a543850b4255171062168181d9ee
Giancarlo Sanchez <gianksp@gmail.com>
2023-05-26 22:41:58 +0100

    Sorting networks asc

--- src/api/networks.js
+++ src/api/networks.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import sortBy from 'lodash/sortBy'
 
 export const loadSupportedNeworks = async () => {
     const result = await axios.get('https://chainid.network/chains.json')
-    return result.data
+    const list  = result.data || []
+    return sortBy(list, 'name')
 }
