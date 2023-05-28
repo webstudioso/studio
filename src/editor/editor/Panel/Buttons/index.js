c3f9aec59674ca7fc92f6a024469661d13dbd45c
Giancarlo Sanchez <gianksp@gmail.com>
2023-05-28 04:37:42 +0100

    Adding back undo redo, it is impossible not to have them

--- src/editor/editor/Panel/Buttons/index.js
+++ src/editor/editor/Panel/Buttons/index.js
@@ -142,40 +142,40 @@ const Plugin = (editor, config) => {
         //     </svg>
         // `,
         // },
-        // {
-        //   id: "undo",
-        //   // className: 'fa fa-undo',
-        //   command: (e) => e.runCommand("core:undo"),
-        //   attributes: { title: "Undo" },
-        //   label: `
-        //     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rotate-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6366f1" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        //       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        //       <path d="M15 4.55a8 8 0 0 0 -6 14.9m0 -4.45v5h-5" />
-        //       <line x1="18.37" y1="7.16" x2="18.37" y2="7.17" />
-        //       <line x1="13" y1="19.94" x2="13" y2="19.95" />
-        //       <line x1="16.84" y1="18.37" x2="16.84" y2="18.38" />
-        //       <line x1="19.37" y1="15.1" x2="19.37" y2="15.11" />
-        //       <line x1="19.94" y1="11" x2="19.94" y2="11.01" />
-        //     </svg>
-        // `,
-        // },
-        // {
-        //   id: "redo",
-        //   // className: 'fa fa-undo',
-        //   command: (e) => e.runCommand("core:redo"),
-        //   attributes: { title: "Redo" },
-        //   label: `
-        //     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rotate-clockwise-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6366f1" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        //       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        //       <path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5" />
-        //       <line x1="5.63" y1="7.16" x2="5.63" y2="7.17" />
-        //       <line x1="4.06" y1="11" x2="4.06" y2="11.01" />
-        //       <line x1="4.63" y1="15.1" x2="4.63" y2="15.11" />
-        //       <line x1="7.16" y1="18.37" x2="7.16" y2="18.38" />
-        //       <line x1="11" y1="19.94" x2="11" y2="19.95" />
-        //     </svg>
-        //   `,
-        // },
+        {
+          id: "undo",
+          // className: 'fa fa-undo',
+          command: (e) => e.runCommand("core:undo"),
+          attributes: { title: "Undo" },
+          label: `
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rotate-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6366f1" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M15 4.55a8 8 0 0 0 -6 14.9m0 -4.45v5h-5" />
+              <line x1="18.37" y1="7.16" x2="18.37" y2="7.17" />
+              <line x1="13" y1="19.94" x2="13" y2="19.95" />
+              <line x1="16.84" y1="18.37" x2="16.84" y2="18.38" />
+              <line x1="19.37" y1="15.1" x2="19.37" y2="15.11" />
+              <line x1="19.94" y1="11" x2="19.94" y2="11.01" />
+            </svg>
+        `,
+        },
+        {
+          id: "redo",
+          // className: 'fa fa-undo',
+          command: (e) => e.runCommand("core:redo"),
+          attributes: { title: "Redo" },
+          label: `
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-rotate-clockwise-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6366f1" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M9 4.55a8 8 0 0 1 6 14.9m0 -4.45v5h5" />
+              <line x1="5.63" y1="7.16" x2="5.63" y2="7.17" />
+              <line x1="4.06" y1="11" x2="4.06" y2="11.01" />
+              <line x1="4.63" y1="15.1" x2="4.63" y2="15.11" />
+              <line x1="7.16" y1="18.37" x2="7.16" y2="18.38" />
+              <line x1="11" y1="19.94" x2="11" y2="19.95" />
+            </svg>
+          `,
+        },
         {
           id: obl,
           command: obl,
