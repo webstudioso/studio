a62b491be27eb8b12f0d1d53c7ba70d7d5cf38ed
Giancarlo Sanchez <gianksp@gmail.com>
2023-11-25 11:18:30 +0000

    Mobile resolution increased in editor for preview

--- src/views/builder/ComponentPropertiesModal/ShadowModal/index.js
+++ src/views/builder/ComponentPropertiesModal/ShadowModal/index.js
@@ -1,15 +1,8 @@
 import { useState } from 'react'
-import { Grid, Popover, Paper, Tooltip, Box } from '@mui/material'
-import constants from 'constant'
+import { Grid, Popover, Paper, Tooltip } from '@mui/material'
 
-const { TAILWIND } = constants
-
-const VARIANTS = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
 const SIZES = ['','-sm', '-md', '-lg', '-xl', '-2xl', '-inner']
 const AVAILABLE = SIZES.map((size) => `shadow${size}` ).flat().concat('shadow-none')
-
-
-// const [availableColors] = useState(TAILWIND.FONT_COLORS.map((color) => VARIANTS.map((variant) => `${color}-${variant}`)).flat())
 
 const ShadowModal = ({ selected, anchorEl, open, onClose }) => {
 
